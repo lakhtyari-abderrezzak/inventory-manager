5d89ca255af5ac66cf31138826a5cb800a379aae
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-09 17:17:22 +0100

    Add Category model with fillable attributes and relationship to products

---
+++ app/Models/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'parent_id',
+        'is_active',
+    ];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+    
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+}
