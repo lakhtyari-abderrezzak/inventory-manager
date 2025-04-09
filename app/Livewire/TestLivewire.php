d53259f7707e5ba8ee9a33a36b252f858c155fe6
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-09 11:55:27 +0100

    Refactor TestLivewire class to improve layout attribute declaration

--- app/Livewire/TestLivewire.php
+++ app/Livewire/TestLivewire.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('components.layouts.test')]
 class TestLivewire extends Component
 {
 
-public $count = 0;
+    public $count = 0;
+
 
     public function increment()
     {
