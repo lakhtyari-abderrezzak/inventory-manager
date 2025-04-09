c5b858d8de6d720aae0073426065e4b2672ab8cc
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-09 17:27:40 +0100

    Add migration for creating products table

---
+++ database/migrations/2025_04_09_110716_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('sku')->unique();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('supplier_id')->nullable()->constrained()->onDelete('set null');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2)->check('price >= 0');
+            $table->decimal('cost_price', 10, 2)->nullable();
+            $table->integer('quantity')->default(0)->check('quantity >= 0');
+            $table->integer('low_stock_alert')->nullable()->check('low_stock_alert >= 0');
+            $table->string('unit')->default('pcs');
+            $table->string('image_path', 255)->nullable();
+            $table->string('barcode')->nullable()->unique();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
