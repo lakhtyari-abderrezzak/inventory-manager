74e279ab8208f763bfa8139fb2227848ecd27681
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-11 00:15:59 +0100

    Refactor database seeder to enhance data generation consistency

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Supplier;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+
+        Category::factory(5)->create();
+        Supplier::factory(5)->create();
+        Product::factory(30)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('123'),
         ]);
     }
 }
