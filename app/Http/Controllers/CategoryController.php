4b4b31c30122fa4aacdbe83ea6e03274a34cab69
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-11 00:16:45 +0100

    Refactor CategoryController for improved code organization and readability

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard.categories.index');
+    }
+
+    public function create()
+    {
+        return view('dashboard.categories.create');
+    }
+
+    public function edit($id)
+    {
+        return view('dashboard.categories.edit', compact('id'));
+    }
 }
