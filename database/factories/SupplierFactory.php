fe1eca39b3f85305a2962e2f07596c82618d373b
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-11 00:15:44 +0100

    Add supplier factory definition with random data generation

--- database/factories/SupplierFactory.php
+++ database/factories/SupplierFactory.php
@@ -17,7 +17,14 @@ class SupplierFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'supplier_name' => $this->faker->company(), // Generate a random supplier name
+            'primary_contact_name' => $this->faker->name(), // Generate a random contact name
+            'email' => $this->faker->unique()->safeEmail(), // Generate a unique email
+            'phone' => $this->faker->unique()->phoneNumber(), // Generate a unique phone number
+            'address' => $this->faker->address(), // Generate a random address
+            'city' => $this->faker->city(), // Generate a random city
+            'country' => $this->faker->country(), // Generate a random country
+            'is_active' => $this->faker->boolean(90), // 90% chance of being active
         ];
     }
 }
