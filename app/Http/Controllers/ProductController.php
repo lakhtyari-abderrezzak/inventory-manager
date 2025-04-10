40d2957b5476928638f0a035ef9c0be4b09112b3
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-11 00:17:38 +0100

    Implement ProductController with index, create, and edit methods for product management

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard.products.index');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function edit($id)
+    {
+        return view('products.edit', compact('id'));
+    }
 }
