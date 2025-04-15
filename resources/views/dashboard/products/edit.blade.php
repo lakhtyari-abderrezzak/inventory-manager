dc55e50e6145bddad820d97c53c18f94615693ff
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-15 02:13:18 +0100

    Refactor edit product view to use layout component and improve structure

--- resources/views/dashboard/products/edit.blade.php
+++ resources/views/dashboard/products/edit.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+<x-layouts.app>
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow rounded-2xl">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-6">Update Product</h1>
+        
+
+        {{-- Form for adding the product using livewire component --}}
+        <livewire:edit-product :productId="$productId" />
+
+    </div>
+</x-layouts.app>
