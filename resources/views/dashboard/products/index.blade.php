09ba0e4cd38b1de26b4fe2880b0507ee89447cfb
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-15 02:13:28 +0100

    Update product index view to include success message and correct add product link

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -2,8 +2,11 @@
     <div class="max-w-7xl mx-auto px-4 py-10">
   <div class="flex justify-between items-center mb-6">
     <h1 class="text-2xl font-bold text-gray-800">Products</h1>
-    <a href="#" class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">+ Add Product</a>
+    <a wire:navigate href="/products/create" class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">+ Add Product</a>
   </div>
+
+  {{-- Success Message if the product is added successfuly --}}
+  <x-success-message />
 
   <livewire:product-list />
 
