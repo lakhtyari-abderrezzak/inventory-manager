fa0bf1b4fa5188e25bccdcd7beba041652c9ee0b
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-16 01:13:48 +0100

    Refactor categories index view for improved layout and structure

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -1,3 +1,15 @@
 <x-layouts.app title="categories">
+    <div class="max-w-7xl mx-auto px-4 py-10">
+
+        <div class="flex justify-between items-center mb-6">
+          <h1 class="text-2xl font-bold text-gray-800">Categories</h1>
+      
+         
+      
+          <a href="#" class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">+ Add Category</a>
+        </div>
     <livewire:category-list />
+
+    
+    </div>
 </x-layouts.app.simp>
