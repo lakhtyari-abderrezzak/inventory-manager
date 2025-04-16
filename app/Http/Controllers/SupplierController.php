ee524c0b76129cc8ad78e45990a2c1ec6c0f78f5
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-16 19:51:25 +0100

    Refactor SupplierController to implement index, show, create, and edit methods

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard.suppliers.index');
+    }
+
+    public function show($supplier)
+    {
+        return view('dashboard.suppliers.show', compact('supplier'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.suppliers.create');
+    }
+
+    public function edit($supplier)
+    {
+        return view('dashboard.suppliers.edit', compact('supplier'));
+    }
 }
