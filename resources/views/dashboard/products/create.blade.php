9fd5cec301c0f4fac3fe96be38ed492fca1c2de6
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-16 19:51:54 +0100

    Add title attribute to layout component in product creation view

--- resources/views/dashboard/products/create.blade.php
+++ resources/views/dashboard/products/create.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app>
+<x-layouts.app title="Add Product">
     <div class="max-w-4xl mx-auto p-6 bg-white shadow rounded-2xl">
         <h1 class="text-2xl font-semibold text-gray-800 mb-6">Add New Product</h1>
         
