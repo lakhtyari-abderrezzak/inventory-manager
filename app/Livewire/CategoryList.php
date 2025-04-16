10ec37beb9b33dfb1e91acfe240c74f2d3852385
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-16 12:45:21 +0100

    Add background color options and improve category deletion feedback

--- app/Livewire/CategoryList.php
+++ app/Livewire/CategoryList.php
@@ -9,13 +9,38 @@ use Livewire\WithPagination;
 class CategoryList extends Component
 {
     use WithPagination;
+
     public $search = '';
+
+    public $bg = [
+        'bg-red-200',
+        'bg-yellow-200',
+        'bg-green-200',
+        'bg-blue-200',
+        'bg-gray-200',
+        'bg-orange-200',
+        'bg-brown-200',
+        'bg-black',
+        'bg-gold-200',
+        'bg-silver-200',
+        'bg-purple-200',
+    ];
+
+    public function delete($id){
+        $category = Category::findOrFail($id);
+
+        $category->delete();
+
+        session()->flash('success', 'Category deleted succeffully!');
+
+    }
+
     public function render()
-    { 
+    {
         $categories = Category::where('name', 'like', '%' . $this->search . '%')
             ->latest()
             ->paginate(6);
-         
+
         return view('livewire.category-list', compact('categories'));
     }
 }
