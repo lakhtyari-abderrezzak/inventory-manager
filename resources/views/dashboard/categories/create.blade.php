423304bd56883cc7fe60d5774ff10ed7b057bd04
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-16 19:15:40 +0100

    Refactor create category view to use layout component and improve structure

--- resources/views/dashboard/categories/create.blade.php
+++ resources/views/dashboard/categories/create.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+<x-layouts.app title="Create Category">
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow rounded-2xl">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-6">Add New Category</h1>
+        
+    <livewire:add-category />
+
+    </div>
+</x-layouts.app >
