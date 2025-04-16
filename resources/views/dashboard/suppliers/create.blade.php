00a98de45dd3ac0e7d2a8db1534d9cfe2a2cfeb6
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-16 19:52:03 +0100

    Add create supplier view with Livewire component

---
+++ resources/views/dashboard/suppliers/create.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app title="Add Supplier">
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow rounded-2xl">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-6">Add New Supplier</h1>
+        
+
+        {{-- Form for adding the supplier using livewire component --}}
+        <livewire:add-supplier />
+    </div>
+</x-layouts.app>
