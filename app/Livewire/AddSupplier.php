6eff46e66541686a4fca961507f55c40e8983731
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-16 19:51:31 +0100

    Add AddSupplier Livewire component for supplier management

---
+++ app/Livewire/AddSupplier.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AddSupplier extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-supplier');
+    }
+}
