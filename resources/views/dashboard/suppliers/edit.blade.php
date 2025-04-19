8096baec78509967ad6eda48ba55a55db32b17f6
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-19 15:04:10 +0100

    Fix formatting in edit supplier view by adding new lines for improved readability

--- resources/views/dashboard/suppliers/edit.blade.php
+++ resources/views/dashboard/suppliers/edit.blade.php
@@ -1,10 +1,12 @@
 <x-layouts.app title="Edit Supplier">
     <div class="max-w-4xl mx-auto p-6 bg-white shadow rounded-2xl">
         <h1 class="text-2xl font-semibold text-gray-800 mb-6">Update Supplier</h1>
-        
+
 
         {{-- Form for adding the product using livewire component --}}
         <livewire:edit-supplier :supplierId="$supplierId" />
 
     </div>
+
+    
 </x-layouts.app>
