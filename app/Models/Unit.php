41ffd913f0149e3864fe2c39c2072ceba3571d53
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-19 16:12:26 +0100

    Refactor Unit model to simplify products relationship and ensure fillable attributes are defined

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -9,4 +9,12 @@ class Unit extends Model
 {
     /** @use HasFactory<\Database\Factories\UnitFactory> */
     use HasFactory;
+    protected $fillable = [
+        'unit_name'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'unit', 'id');
+    }
 }
