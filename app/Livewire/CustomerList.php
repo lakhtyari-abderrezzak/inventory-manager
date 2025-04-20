d008206a70f0837f6240f6408c0bb0301f07cd0b
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-20 15:22:17 +0100

    feat: add delete method to CustomerList component with success and error notifications

--- app/Livewire/CustomerList.php
+++ app/Livewire/CustomerList.php
@@ -9,6 +9,17 @@ class CustomerList extends Component
 {
     public $search = '';
 
+    public function delete($customerId)
+    {
+        $customer = Customer::find($customerId);
+        if ($customer) {
+            $customer->delete();
+            $this->dispatch('flash', type: 'success', message: 'Customer deleted successfully!');
+        } else {
+           $this->dispatch('flash', type: 'error', message: 'Customer not found!');
+        }
+    }
+
     public function render()
     {
         $customers = Customer::where('name', 'like', '%' . $this->search . '%')
@@ -18,6 +29,8 @@ class CustomerList extends Component
             ->orWhere('city', 'like', '%' . $this->search . '%')
             ->orWhere('country', 'like', '%' . $this->search . '%')
             ->paginate(10);
+
+
         return view('livewire.customer-list', compact(
             'customers'
         ));
