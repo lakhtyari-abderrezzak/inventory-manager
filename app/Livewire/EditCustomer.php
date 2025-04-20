4bec2e3ac3b08eea37cadf5adaa957aa45dec994
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-20 15:22:26 +0100

    feat: create EditCustomer Livewire component for customer editing functionality

---
+++ app/Livewire/EditCustomer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Customer;
+use Livewire\Component;
+
+class EditCustomer extends Component
+{
+    public $customer;
+    public $customerId;
+    public $name , $email, $phone, $city, $address, $country ;
+
+    public function mount($customerId){
+        $this->customerId = $customerId;
+        $this->customer = Customer::findOrFail($customerId);
+
+        $this->name = $this->customer->name;
+        $this->email = $this->customer->email;
+        $this->phone = $this->customer->phone;
+        $this->city = $this->customer->city;
+        $this->address = $this->customer->address;
+        $this->country = $this->customer->country;
+    }
+
+    protected $rules= [
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|max:255',
+        'phone' => 'required|string|max:255',
+        'city' => 'required|string|max:255',
+        'address' => 'required|string|max:255',
+        'country' => 'required|string|max:255',
+
+    ];
+    public function submit(){
+        $this->validate();
+
+        $this->customer->update([
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'city' => $this->city,
+            'address' => $this->address,
+            'country' => $this->country,
+        ]);
+
+        $this->dispatch('flash', type: 'success', message: 'Customer updated!');
+
+        $this->dispatch('redirect',
+            url : route('customers.index', ['customer' => $this->customer->id])
+        );
+    }
+    
+    public function render()
+    {
+        return view('livewire.edit-customer');
+    }
+}
