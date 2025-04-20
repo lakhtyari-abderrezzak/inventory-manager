51469c59056744ba29d74f4f96d778335670950f
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-20 12:34:05 +0100

    Remove unused success-message component. Re[placed with sweet alert

--- resources/views/components/success-message.blade.php
+++
@@ -1,6 +0,0 @@
-@if (session()->has('message'))
-    <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)"
-        class="bg-green-100 text-green-800 px-4 py-2 rounded mb-4 transition duration-500">
-        {{ session('message') }}
-    </div>
-@endif
