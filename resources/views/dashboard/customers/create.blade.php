05cf5712be31765474fe37e22336eeeea1603101
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-20 14:28:55 +0100

    feat(ui): added customer creation form with Livewire

--- resources/views/dashboard/customers/create.blade.php
+++ resources/views/dashboard/customers/create.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+<x-layouts.app title="Add Customer">
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow rounded-2xl">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-6">Add New Customer</h1>
+        
+
+        {{-- Form for adding the Customer using livewire component --}}
+        <livewire:add-customer />
+
+    </div>
+</x-layouts.app>
