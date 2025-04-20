d877b7d3a32b3e4d322a14277b2ad842e7449f58
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-20 15:22:35 +0100

    feat: implement EditCustomer view with Livewire component integration

--- resources/views/dashboard/customers/edit.blade.php
+++ resources/views/dashboard/customers/edit.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+<x-layouts.app title="Edit Customer">
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow rounded-2xl">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-6">Update Customer</h1>
+        
+
+        {{-- Form for adding the Customer using livewire component --}}
+        <livewire:edit-customer :customerId="$customerId" />
+
+    </div>
+</x-layouts.app>
