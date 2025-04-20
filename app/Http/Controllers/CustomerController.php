4e5950c7715622525ad419e58a08cb2cd0a342f7
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-20 14:24:52 +0100

    Implement customer resource methods in CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard.customers.index');
+    }
+
+    public function create()
+    {
+        return view('dashboard.customers.create');
+    }
+    
+    public function show(Customer $customer)
+    {
+        return view('dashboard.customers.show', compact('customer'));
+    }
+    
+    public function edit(Customer $customer)
+    {
+        return view('dashboard.customers.edit', compact('customer'));
+    }
+   
 }
