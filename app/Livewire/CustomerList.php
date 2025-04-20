93a15f27f7e05ee8ea0b35fbea839363962f6ac7
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-20 14:25:03 +0100

    Implement search functionality in CustomerList component

--- app/Livewire/CustomerList.php
+++ app/Livewire/CustomerList.php
@@ -3,11 +3,23 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Customer;
 
 class CustomerList extends Component
 {
+    public $search = '';
+
     public function render()
     {
-        return view('livewire.customer-list');
+        $customers = Customer::where('name', 'like', '%' . $this->search . '%')
+            ->orWhere('email', 'like', '%' . $this->search . '%')
+            ->orWhere('phone', 'like', '%' . $this->search . '%')
+            ->orWhere('address', 'like', '%' . $this->search . '%')
+            ->orWhere('city', 'like', '%' . $this->search . '%')
+            ->orWhere('country', 'like', '%' . $this->search . '%')
+            ->paginate(10);
+        return view('livewire.customer-list', compact(
+            'customers'
+        ));
     }
 }
