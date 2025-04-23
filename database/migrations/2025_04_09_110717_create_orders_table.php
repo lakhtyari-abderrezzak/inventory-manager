cfbc3541be240a1305e57427e1ee696daa8eb174
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-23 01:15:55 +0100

    feat(migration): add orders table with foreign keys to customers and products, including quantity, pricing, status, payment, and address fields

---
+++ database/migrations/2025_04_09_110717_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('total_price', 10, 2);
+            $table->string('status')->default('pending'); // e.g., pending, completed, canceled
+            $table->string('payment_method')->nullable(); 
+            $table->string('shipping_address')->nullable();
+            $table->string('billing_address')->nullable();
+            $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
