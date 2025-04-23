2d15c3cb3f15454e5f348f038375c7f27621a297
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-23 02:02:54 +0100

    feat: implement delete method in order-list Livewire component to remove orders and related items

--- app/Livewire/OrderList.php
+++ app/Livewire/OrderList.php
@@ -9,6 +9,20 @@ class OrderList extends Component
 {
     public $search = '';
 
+    public function delete($id)
+    {
+        $order = Order::findOrFail($id);
+        // Delete associated order items
+        foreach ($order->items as $item) {
+            $item->delete();
+        }
+        // Delete the order itself
+        $order->delete();
+
+        $this->dispatch('flash', type: 'success', message: 'Order deleted successfully!');
+
+    }
+
     public function render()
     {
         // Fetch all orders from the database
@@ -16,7 +30,7 @@ class OrderList extends Component
             ->where('order_number', 'like', '%' . $this->search . '%')
             ->latest()
             ->paginate(10);
-    
+
         return view('livewire.order-list', compact('orders'));
     }
 }
