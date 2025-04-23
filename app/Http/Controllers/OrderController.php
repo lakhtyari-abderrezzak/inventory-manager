b0e53fc6c5ec344b2d44af8d8ee2205aac52d928
Abderrazzak lakhtyari <lakhtyari.abderrazzak@gmail.com>
2025-04-23 01:17:33 +0100

    feat(controller): implment OrderController with methods to view, create, show, and edit orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        // Fetch all orders from the database
+        $orders = Order::with('customer', 'items.product')->latest()->paginate(10);
+        return view('dashboard.orders.index', compact('orders'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.orders.create');
+    }
+
+    public function show($id)
+    {
+        return view('dashboard.orders.show', compact('id'));
+    }
+    
+    public function edit($id)
+    {
+        return view('dashboard.orders.edit', compact('id'));
+    }
+    
 }
